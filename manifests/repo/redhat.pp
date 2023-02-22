99dd1f2b41de09da5aaaa29b5f435b233935f43b
sssadw <anthony.worrall@reading.ac.uk>
2023-02-22 15:34:19 +0000

    new mirrorlist url for releasever 8 or greater

--- manifests/repo/redhat.pp
+++ manifests/repo/redhat.pp
@@ -14,21 +14,42 @@ class php::repo::redhat (
     default       => '$releasever',  # Yum var
   }
 
-  yumrepo { 'remi':
-    descr      => 'Remi\'s RPM repository for Enterprise Linux $releasever - $basearch',
-    mirrorlist => "https://rpms.remirepo.net/enterprise/${releasever}/remi/mirror",
-    enabled    => 1,
-    gpgcheck   => 1,
-    gpgkey     => 'https://rpms.remirepo.net/RPM-GPG-KEY-remi',
-    priority   => 1,
-  }
+  if (versioncmp($releasever,'8') < 0) {
+    yumrepo { 'remi':
+      descr      => 'Remi\'s RPM repository for Enterprise Linux $releasever - $basearch',
+      mirrorlist => "https://rpms.remirepo.net/enterprise/${releasever}/remi/mirror",
+      enabled    => 1,
+      gpgcheck   => 1,
+      gpgkey     => 'https://rpms.remirepo.net/RPM-GPG-KEY-remi',
+      priority   => 1,
+    }
 
-  yumrepo { 'remi-php56':
-    descr      => 'Remi\'s PHP 5.6 RPM repository for Enterprise Linux $releasever - $basearch',
-    mirrorlist => "https://rpms.remirepo.net/enterprise/${releasever}/php56/mirror",
-    enabled    => 1,
-    gpgcheck   => 1,
-    gpgkey     => 'https://rpms.remirepo.net/RPM-GPG-KEY-remi',
-    priority   => 1,
+    yumrepo { 'remi-php56':
+      descr      => 'Remi\'s PHP 5.6 RPM repository for Enterprise Linux $releasever - $basearch',
+      mirrorlist => "https://rpms.remirepo.net/enterprise/${releasever}/php56/mirror",
+      enabled    => 1,
+      gpgcheck   => 1,
+      gpgkey     => 'https://rpms.remirepo.net/RPM-GPG-KEY-remi',
+      priority   => 1,
+    }
+  } else {
+    yumrepo { 'remi':
+      descr      => 'Remi\'s RPM repository for Enterprise Linux $releasever - $basearch',
+      mirrorlist => "http://cdn.remirepo.net/enterprise/${releasever}/remi/$basearch/mirror",
+      enabled    => 1,
+      gpgcheck   => 1,
+      gpgkey     => 'https://rpms.remirepo.net/RPM-GPG-KEY-remi',
+      priority   => 1,
+    }
+
+    yumrepo { 'remi-php74':
+      descr      => 'Remi\'s PHP 7.4 RPM repository for Enterprise Linux $releasever - $basearch',
+      mirrorlist => "http://cdn.remirepo.net/enterprise/${releasever}/remi/php74/$basearch/mirror"
+      enabled    => 1,
+      gpgcheck   => 1,
+      gpgkey     => 'https://rpms.remirepo.net/RPM-GPG-KEY-remi',
+      priority   => 1,
+    }
+
   }
 }
